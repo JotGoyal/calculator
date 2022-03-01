6138a13cd735141d3ce90b945fc80947f10210a9
JotGoyal <goyal.misspriya@gmail.com>
2022-03-01 19:54:48 +0530

    first commit

---
+++ calc.js
@@ -0,0 +1,12 @@
+function calc(operation){
+    let a = document.getElementById('first').value;
+    let b = document.getElementById('second').value;
+    let output;
+    if(operation=='Add'){
+        output=`Sum of number is ${Number(a)+Number(b)}`
+    }
+    else if(operation=='Sub'){
+        output=`Sub of number is ${Number(a)+Number(b)}`
+    }
+    document.getElementsByClassName('output')[0].innerText=output;
+}
